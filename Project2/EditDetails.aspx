92f03337f25636fff6b468ec530cb3d4aa0a46fd
Renier Botha <renier@luyt7.co.za>
2021-11-21 18:30:08 +0200

    Editing details

--- Project2/EditDetails.aspx
+++ Project2/EditDetails.aspx
@@ -5,11 +5,48 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div class="auto-style1">
+            <asp:Label ID="lblHeading" runat="server" Font-Bold="True" Font-Size="XX-Large" Text="EDIT YOUR DETAILS"></asp:Label>
         </div>
+        <p>
+            <asp:HyperLink ID="HyperLink1" runat="server" Font-Bold="True" Font-Italic="True" Font-Size="Medium" Font-Underline="True" ForeColor="Blue" NavigateUrl="~/Main.aspx">Main Menu</asp:HyperLink>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:RadioButton ID="radEmail" runat="server" AutoPostBack="True" Font-Bold="True" Font-Size="Medium" GroupName="Decision" OnCheckedChanged="radEmail_CheckedChanged" Text="Email" />
+&nbsp;
+            <asp:RadioButton ID="RadCell" runat="server" AutoPostBack="True" Font-Bold="True" Font-Size="Medium" GroupName="Decision" OnCheckedChanged="radEmail_CheckedChanged" Text="Cellphone Number" />
+&nbsp;
+            <asp:RadioButton ID="radPassword" runat="server" AutoPostBack="True" Font-Bold="True" Font-Size="Medium" GroupName="Decision" OnCheckedChanged="radEmail_CheckedChanged" Text="Password" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="lblEnter" runat="server" Font-Bold="True" Font-Size="Medium" Visible="False"></asp:Label>
+            <asp:TextBox ID="txtEnter" runat="server" Font-Bold="False" Font-Size="Medium" Visible="False"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtEnter" Enabled="False" ErrorMessage="RequiredFieldValidator" Font-Bold="True" Font-Size="Medium" ForeColor="Red" Visible="False">*</asp:RequiredFieldValidator>
+&nbsp;<asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="txtEnter" Enabled="False" ErrorMessage="RegularExpressionValidator" Font-Bold="True" Font-Size="Medium" ForeColor="Red" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" Visible="False">Please enter a valid email</asp:RegularExpressionValidator>
+        </p>
+        <p>
+            <asp:Label ID="lblConfirm" runat="server" Font-Bold="True" Font-Size="Medium" Visible="False"></asp:Label>
+            <asp:TextBox ID="txtConf" runat="server" Font-Bold="False" Font-Size="Medium" Visible="False"></asp:TextBox>
+            <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToCompare="txtEnter" ControlToValidate="txtConf" Enabled="False" ErrorMessage="CompareValidator" Font-Bold="True" ForeColor="Red" Visible="False">Entered data does not match</asp:CompareValidator>
+        </p>
+        <p>
+            <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update Details" Visible="False" Width="150px" />
+        </p>
+        <p>
+            <asp:Label ID="lblDisplay" runat="server" Font-Bold="True" Font-Size="Large" Visible="False"></asp:Label>
+        </p>
     </form>
 </body>
 </html>
--- Project2/EditDetails.aspx.cs
+++ Project2/EditDetails.aspx.cs
@@ -4,14 +4,145 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Project2
 {
     public partial class EditDetails : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        public SqlConnection conn;
+        public SqlCommand com;
+        public DataSet ds;
+        public SqlDataAdapter adapt;
+        public SqlDataReader dataRead;
+        public DataTable dt;
+        public string state;
 
+        private void Test(string myState, string sql, string details)
+        {
+            string thisCon = Session["MyDB"].ToString();
+            int userid = int.Parse(Session["UserID"].ToString());
+
+            conn = new SqlConnection(thisCon);
+            conn.Open();
+            adapt = new SqlDataAdapter(myState, conn);
+            dt = new DataTable();
+            adapt.Fill(dt);
+
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                lblDisplay.ForeColor = System.Drawing.Color.Red;
+                lblDisplay.Text = "Entered data already exists";
+                txtEnter.Text = "";
+                txtConf.Text = "";
+            }
+            else
+            {
+                com = new SqlCommand(sql, conn);
+                com.Parameters.AddWithValue("@details", details);
+                com.Parameters.AddWithValue("@userid", userid);
+                com.ExecuteNonQuery();
+
+                lblDisplay.ForeColor = System.Drawing.Color.Blue;
+                lblDisplay.Text = "Successfully updated";
+                txtEnter.Text = "";
+                txtConf.Text = "";
+                conn.Close();
+            }
+        }
+
+        private void Selection(string text1, string text2)
+        {
+            lblEnter.Visible = true;
+            lblEnter.Text = text1;
+            lblConfirm.Visible = true;
+            lblConfirm.Text = text2;
+            txtEnter.Visible = true;
+            txtConf.Visible = true;
+            btnUpdate.Visible = true;
+            lblDisplay.Visible = true;
+            RequiredFieldValidator1.Visible = true;
+            RequiredFieldValidator1.Enabled = true;
+            CompareValidator1.Visible = true;
+            CompareValidator1.Enabled = true;
+            txtEnter.Text = "";
+            txtConf.Text = "";
+        }
+
+        protected void radEmail_CheckedChanged(object sender, EventArgs e)
+        {
+            if(this.radEmail.Checked)
+            {
+                RegularExpressionValidator1.Visible = true;
+                RegularExpressionValidator1.Enabled = true;
+                txtEnter.TextMode = TextBoxMode.SingleLine;
+                txtConf.TextMode = TextBoxMode.SingleLine;
+                Selection("Please enter your new email", "Please confirm your new email");
+            }
+            else if (this.RadCell.Checked)
+            {
+                RegularExpressionValidator1.Visible = false;
+                RegularExpressionValidator1.Enabled = false;
+                txtEnter.TextMode = TextBoxMode.SingleLine;
+                txtConf.TextMode = TextBoxMode.SingleLine;
+                Selection("Please enter your new cellphone number", "Please confirm your new cellphone number");
+            }
+            else if (this.radPassword.Checked)
+            {
+                txtEnter.TextMode = TextBoxMode.Password;
+                txtConf.TextMode = TextBoxMode.Password;
+                RegularExpressionValidator1.Visible = false;
+                RegularExpressionValidator1.Enabled = false;
+                Selection("Please enter your new password", "Please confirm your new password");
+            }
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string details = txtConf.Text;
+
+            if (radEmail.Checked)
+            {
+                Test("SELECT Count(*) FROM Users WHERE Email = '" + details + "'", "Update Users SET Email = @details WHERE UserID = @userid", details);
+            }
+            else if(RadCell.Checked)
+            {
+                int tesv;
+                if (details.Length != 10)
+                {
+                    lblDisplay.ForeColor = System.Drawing.Color.Red;
+                    lblDisplay.Text = "Incorrect length, please try again";
+                    txtEnter.Text = "";
+                    txtConf.Text = "";
+                } else if (int.TryParse(details, out tesv) == false)
+                {
+                    lblDisplay.ForeColor = System.Drawing.Color.Red;
+                    lblDisplay.Text = "Please enter a valid cellphone number";
+                    txtEnter.Text = "";
+                    txtConf.Text = "";
+                }
+                else
+                {
+                    Test("SELECT Count(*) FROM Users WHERE CellNr = '" + details + "'", "Update Users SET CellNr = @details WHERE UserID = @userid", details);
+                }
+            }
+            else if(radPassword.Checked)
+            {
+                if (details.Length < 8)
+                {
+                    lblDisplay.ForeColor = System.Drawing.Color.Red;
+                    lblDisplay.Text = "Password to short, please try again";
+                    txtEnter.Text = "";
+                    txtConf.Text = "";
+                }
+                else
+                {
+                    Test("SELECT Count(*) FROM Users WHERE Password = '" + details + "'", "Update Users SET Password = @details WHERE UserID = @userid", details);
+                }
+            }
         }
     }
 }
